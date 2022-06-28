1665082e0b2c822d0d833ae877655aa88e6c4f8f
sondossamii <sondos.sami.abuzeid@gmail.com>
2022-06-29 01:45:18 +0200

    Add 'Solutions' Section

---
+++ script.js
@@ -0,0 +1,18 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsive: {
+    0: {
+      items: 1.25,
+    },
+    500: {
+      items: 1.5,
+    },
+    768: {
+      items: 1.7,
+    },
+    992: {
+      items: 1.35,
+    },
+  },
+});
